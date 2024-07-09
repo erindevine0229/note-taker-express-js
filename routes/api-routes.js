9bbb4b5012843bb55c23d280409520c9e4ab2deb
Erin DeVine <erinj291@gmail.com>
2024-07-09 18:29:34 -0400

    started get/post requests for api router

---
+++ routes/api-routes.js
@@ -0,0 +1,29 @@
+const router = require('express'). Router();
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+
+router.get('/api/notes', (req, res) => {
+    fs.readFile('db.json', 'utf8', (err, noteData) => {
+        if (err) {
+            console.error('Error occurred when reading data', err);
+        }
+    })
+    res.json(JSON.parse(data));
+});
+
+
+router.post('/api/notes', (req, res) => {
+    fs.readFile('db.json', 'utf8', (err, noteData) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+    
+    const currentInfo = json.parse(noteData);
+
+    const newInfo = {
+       id: uuidv4(),
+       title: req.body.title,
+       text: req.body.text,
+    };
+})
